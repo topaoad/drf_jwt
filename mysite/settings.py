d3813b4d2b286f19c5dcd398d3d261ad889f9a18
toproad <tattun0817@gmail.com>
2023-04-09 11:30:27 +0900

    セキュリティ対策

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5*yy*yq=5wc@2o=ve_1)9*q1j+!ine*s75t-90_0%mkf@v#r1#'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
